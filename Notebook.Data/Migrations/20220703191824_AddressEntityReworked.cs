f578f99d1c341d8ca25efb26dcdedeb8c0f90bb0
Denys Boryhin <denys.boryhin@nixsolutions.com>
2022-07-03 22:20:43 +0300

    Bug with unique Address Index has been fixed by additional migration

---
+++ Notebook.Data/Migrations/20220703191824_AddressEntityReworked.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notebook.Data.Migrations
+{
+    public partial class AddressEntityReworked : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Address_Index",
+                table: "Address");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_Index",
+                table: "Address",
+                column: "Index",
+                unique: true);
+        }
+    }
+}
